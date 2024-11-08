9c758f78b3390713336d2714e75839c366bf2eb2
keepscode-snow <jorge.dominguez06@inacapmail.cl>
2024-11-08 12:03:28 -0300

    Add files via upload

---
+++ ev3 mate/src/components/Calculator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="calculator">
+    <CalculatorDisplay :value="displayValue" />
+    <div class="calculator-buttons">
+      <CalculatorButton v-for="button in buttons" :key="button" :label="button" @press="handleButtonPress" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CalculatorDisplay from './CalculatorDisplay.vue';
+import CalculatorButton from './CalculatorButton.vue';
+
+const displayValue = ref('0');
+const buttons = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'];
+
+const handleButtonPress = (label) => {
+  if (label === '=') {
+    displayValue.value = eval(displayValue.value).toString();
+  } else {
+    displayValue.value += label;
+  }
+};
+</script>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2em;
+}
+
+.calculator-buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 0.5em;
+}
+</style>
---
+++ ev3 mate/src/components/CalculatorButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="handleClick" class="calculator-button">
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  }
+});
+
+const emits = defineEmits(['press']);
+
+const handleClick = () => {
+  emits('press', props.label);
+};
+</script>
+
+<style scoped>
+.calculator-button {
+  background: #444;
+  border: none;
+  color: #fff;
+  font-size: 1.5em;
+  margin: 0.2em;
+  padding: 1em;
+  width: 3em;
+  height: 3em;
+}
+</style>
